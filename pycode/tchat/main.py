f0d78124cb098a384f69f359912edbc7b2a0c822
alianwaar73 <ali.github.anwaar73@gmail.com>
2025-05-21 17:28:28 +0500

    added the feature of memory, both on going chats and among app bootups

--- pycode/tchat/main.py
+++ pycode/tchat/main.py
@@ -1,7 +1,7 @@
 # HumanMessagePromptTemplate: For user input
 # ChatPromptTemplate: ??
 
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
 
 # import langchain.llms implies a completion model the following is different
 
@@ -11,21 +11,38 @@ from langchain.chains import LLMChain
 # Importing our API key from the environment variable
 from dotenv import load_dotenv
 
+# Incorporating memory into our application so that pervious messages and responses are saved and can be referred to and conversations can go on naturally. ConversationBufferMemory is to store history of an on going chat.
+# Whereas FileChatMessageHistory is to store conversations between boots of the program or application!
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+
 load_dotenv()
 
 chat = ChatOpenAI()
 
+memory = ConversationBufferMemory(
+        # Whereas messages.json file acts as a DB that stores
+        # old chats that can be referred to whenever the application is booted. The constructor FileChatMessageHistory automatically creates the file with the name provided to it as the input argument.
+        chat_memory=FileChatMessageHistory("messages.json"),
+        memory_key="messages", 
+        return_messages=True
+        )
+
 # Start of a chain
 prompt = ChatPromptTemplate(
-        input_variables=["content"],
+        # The variable messages is so that previously stored messages can be referred to
+        input_variables=["content", "messages"],
         messages=[
+            # The following line is for the aspect of memory
+            MessagesPlaceholder(variable_name="messages"),
             HumanMessagePromptTemplate.from_template("{content}")
             ]
         )
 
 chain = LLMChain(
         llm=chat,
-        prompt=prompt
+        prompt=prompt,
+        # Wiring the memory in
+        memory=memory
         )
 
 while True:
